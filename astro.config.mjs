107d73cc7af9f917293808d56e20ed0ce00ef729
Isaac <zaragozamendozaisaac@outlook.es>
2024-09-08 20:47:51 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), keystatic()],
-  output: "hybrid",
+  output: "server",
   redirects: {
     "/admin": "/keystatic"
   },
