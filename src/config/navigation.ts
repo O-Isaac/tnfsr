c8960732a6cd346bd5eac47249e21d02b7669057
Isaac <zaragozamendozaisaac@outlook.es>
2024-09-08 20:45:05 +0200

    Update navigation.ts

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -1,6 +1,6 @@
 export const NavbarLinks = [
     { name: "Home", href: "/" },
-    { name: "Events", href: "/events" },
+    { name: "Categories", href: "/categories" },
     { name: "Services", href: "/services" },
     { name: "Contact", href: "/contact" }
 ]
