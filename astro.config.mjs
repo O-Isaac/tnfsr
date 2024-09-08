908f806a76a88722cb7edf1229020c22af951595
O-Isaac <soporte@proyectograndorder.es>
2024-09-08 20:16:38 +0200

    Adapter serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import keystatic from '@keystatic/astro'
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
